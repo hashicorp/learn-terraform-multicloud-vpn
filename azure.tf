a2d5ed0e360fc8ec7bea3f000074a708ed8e157f
Rachel Sharp <rachel@hashicorp.com>
2022-02-24 14:36:20 -0500

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -4,80 +4,70 @@ provider "azurerm" {
 
 terraform {}
 
-resource "azurerm_resource_group" "resource_group" {
-  name     = "resource_group"
+resource "azurerm_resource_group" "learn_vpn_rg" {
+  name     = "learn_vpn_rg"
   location = "westus2"
 }
 
 ### Network
 
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
+resource "azurerm_virtual_network" "learn_vpn_vnet" {
+  name                = "learn_vpn_vnet"
+  location            = azurerm_resource_group.learn_vpn_rg.location
+  resource_group_name = azurerm_resource_group.learn_vpn_rg.name
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "subnet_1" {
-  name                 = "subnet_1"
-  resource_group_name  = azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+resource "azurerm_subnet" "learn_vpn_subnet" {
+  name                 = "learn_vpn_subnet"
+  resource_group_name  = azurerm_resource_group.learn_vpn_rg.name
+  virtual_network_name = azurerm_virtual_network.learn_vpn_vnet.name
   address_prefix       = "10.0.1.0/24"
 }
 
-resource "azurerm_subnet" "subnet_gateway" {
+resource "azurerm_subnet" "learn_vpn_gwsubnet" {
 
   name                 = "GatewaySubnet"
-  resource_group_name  = azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.learn_vpn_rg.name
+  virtual_network_name = azurerm_virtual_network.learn_vpn_vnet.name
   address_prefix       = "10.0.2.0/24"
 }
 
-resource "azurerm_public_ip" "public_ip_1" {
-  name                = "virtual_network_gateway_public_ip_1"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
+resource "azurerm_public_ip" "learn_vpn_publicip" {
+  name                = "learn_vpn_publicip"
+  location            = azurerm_resource_group.learn_vpn_rg.location
+  resource_group_name = azurerm_resource_group.learn_vpn_rg.name
 
 
   allocation_method = "Dynamic"
 }
 
-resource "azurerm_public_ip" "public_ip_2" {
-  name                = "virtual_network_gateway_public_ip_2"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-
-  # Public IP needs to be dynamic for the Virtual Network Gateway
-  allocation_method = "Dynamic"
-}
 
 
-
-### Virtual Machine
-resource "azurerm_public_ip" "public_ip_vm" {
-  name                = "public_ip_vm"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
+resource "azurerm_public_ip" "learn_vpn_vm-publicip" {
+  name                = "learn_vpn_public_ip_vm"
+  location            = azurerm_resource_group.learn_vpn_rg.location
+  resource_group_name = azurerm_resource_group.learn_vpn_rg.name
 
   allocation_method = "Static"
 }
 
-resource "azurerm_network_interface" "network_interface_vm" {
+resource "azurerm_network_interface" "learn_vpn_vm-int" {
   name                = "network_interface_vm"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.learn_vpn_rg.location
+  resource_group_name = azurerm_resource_group.learn_vpn_rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet_1.id
+    subnet_id                     = azurerm_subnet.learn_vpn_gwsubnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip_vm.id
+    public_ip_address_id          = azurerm_public_ip.learn_vpn_vm-publicip.id
   }
 }
 
 data "template_file" "script" {
-  template = "${file("${path.module}/scripts/backend_hashicups.yaml")}"
+  template = "${file("./scripts/backend_hashicups.yaml")}"
 }
 
 data "template_cloudinit_config" "backend" {
@@ -90,16 +80,16 @@ data "template_cloudinit_config" "backend" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm" {
+resource "azurerm_linux_virtual_machine" "learn_vpn_vm" {
   name                = "vm"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.learn_vpn_rg.location
+  resource_group_name = azurerm_resource_group.learn_vpn_rg.name
   size                = "Standard_F2"
   admin_username      = "ubuntu"
   admin_password = "Plankton123!"
 
   network_interface_ids = [
-    azurerm_network_interface.network_interface_vm.id,
+    azurerm_network_interface.learn_vpn_vm-int.id,
   ]
 
   os_disk {
@@ -122,9 +112,9 @@ resource "azurerm_linux_virtual_machine" "vm" {
 ### Outputs
 
 output "azure_vm_public_ip" {
-  value = azurerm_linux_virtual_machine.vm.public_ip_address
+  value = azurerm_linux_virtual_machine.learn_vpn_vm.public_ip_address
 }
 
 output "azure_vm_private_ip" {
-  value = azurerm_linux_virtual_machine.vm.private_ip_address
+  value = azurerm_linux_virtual_machine.learn_vpn_vm.private_ip_address
 }
